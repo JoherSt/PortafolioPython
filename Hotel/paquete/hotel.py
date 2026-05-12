9b2b42b74bbfba57f4df60172c1980d8e23122f3
Joher Franco <joherstivenfrancoa@gmail.com>
2026-05-12 16:46:08 -0500

    Actualizacion de reporte

--- Hotel/paquete/hotel.py
+++ Hotel/paquete/hotel.py
@@ -77,7 +77,7 @@ def reservar_habitacion():
             print("Reserva realizada con exito: ")
             print(f"El total es: {total}")
             return
-    if not encontrada:
+        if not encontrada:
             print("La habitacion no existe")
 
 
